2b1ddd01e29bf9cb969ba6732cca4cebf5742dde
Gustavo Rodrigues <gustavo.hwmanz@gmail.com>
2022-07-05 11:11:04 -0300

    Added shiba model for testing

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+const loader = new GLTFLoader()
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.querySelector('#app').appendChild( renderer.domElement );
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize( window.innerWidth, window.innerHeight )
+document.querySelector('#app').appendChild( renderer.domElement )
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+let shiba
+loader.load('./models/shiba/scene.gltf', gltf => {
+  shiba = gltf.scene
+  scene.add(shiba)
+}, undefined, error => {
+  console.error(error)
+})
 
 camera.position.z = 5;
 
 function animate() {
-	requestAnimationFrame( animate );
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
+	requestAnimationFrame( animate )
+  shiba.rotation.y += 0.01
+  // shiba.rotation.z += 0.01
+	renderer.render( scene, camera )
 }
-animate();
+animate()
